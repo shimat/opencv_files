2b1c75f7320dcd5ef1a2d6979860684adc5da378
shimat <shima@sansan.com>
2020-04-02 08:55:30 +0900

    python off

--- build_windows.ps1
+++ build_windows.ps1
@@ -18,15 +18,21 @@ function BuildForWindows($platform, $vcpkgPath, $runMsbuild) {
           -D CMAKE_INSTALL_PREFIX=${buildDirectory}/install `
           -D INSTALL_C_EXAMPLES=ON `
           -D INSTALL_PYTHON_EXAMPLES=OFF `
+          -D BUILD_ANDROID_PROJECTS=OFF `
+          -D BUILD_ANDROID_EXAMPLES=OFF `
           -D BUILD_DOCS=OFF `
+          -D BUILD_WITH_DEBUG_INFO=OFF `
           -D BUILD_EXAMPLES=OFF `
           -D BUILD_TESTS=OFF `
           -D BUILD_PERF_TESTS=OFF `
           -D BUILD_JAVA=OFF `
           -D BUILD_opencv_apps=OFF `
-          -D BUILD_opencv_gapi=OFF `
           -D BUILD_opencv_datasets=OFF `
+          -D BUILD_opencv_gapi=OFF `
           -D BUILD_opencv_java_bindings_generator=OFF `
+          -D BUILD_opencv_js=OFF `
+          -D BUILD_opencv_python2=OFF `
+          -D BUILD_opencv_python3=OFF `
           -D BUILD_opencv_python_bindings_generator=OFF `
           -D BUILD_opencv_python_tests=OFF `
           -D BUILD_opencv_ts=OFF `
@@ -43,7 +49,11 @@ function BuildForWindows($platform, $vcpkgPath, $runMsbuild) {
           -D BUILD_SHARED_LIBS=OFF ../opencv 
 
     if ($runMsbuild) {
-        msbuild INSTALL.vcxproj /t:build /p:configuration=Release /p:platform=$msbuildPlatform -maxcpucount -ErrorAction Stop
+        # Developer Powershell for VS 2019 
+        # Path: C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -noe -c "&{Import-Module """C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell cebe9bd5}"
+        # WorkDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\
+
+        msbuild INSTALL.vcxproj /t:build /p:configuration=Release /p:platform=$msbuildPlatform -maxcpucount
         ls
     }
 
